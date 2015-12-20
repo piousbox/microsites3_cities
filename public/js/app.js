62fdfe6a2eed989133e8672f34d53d9d6f99bb13
Victor Piousbox <piousbox@gmail.com>
2015-12-20 08:47:33 -0800

    was a long time ago

--- public/js/app.js
+++ public/js/app.js
@@ -32,7 +32,7 @@ config(['$routeProvider', '$locationProvider', '$stateProvider', '$urlRouterProv
     $stateProvider
         .state('home', {
             url: '/',
-            templateUrl: "partials/cities/list.html",
+            templateUrl: "partials/welcome/home.html",
             resolve: {
                 City: ['City', function(City) {
                     return City;
@@ -52,4 +52,71 @@ config(['$routeProvider', '$locationProvider', '$stateProvider', '$urlRouterProv
             template: "<h5>About template</h5>"
         });
     
-}]);
+}]).
+constant('AUTH_EVENTS', {
+  loginSuccess: 'auth-login-success',
+  loginFailed: 'auth-login-failed',
+  logoutSuccess: 'auth-logout-success',
+  sessionTimeout: 'auth-session-timeout',
+  notAuthenticated: 'auth-not-authenticated',
+  notAuthorized: 'auth-not-authorized'
+}).
+constant('USER_ROLES', {
+  all: '*',
+  admin: 'admin',
+  editor: 'editor',
+  guest: 'guest'
+}).
+factory('AuthService', function ($http, Session) {
+  var authService = {};
+ 
+  authService.login = function (credentials) {
+    return $http
+      .post('http://api.local/en/users/sign_in', { user: credentials })
+      .then(function (res) {
+        Session.create(res.data.id, res.data.user.id,
+                       res.data.user.role);
+        return res.data.user;
+      });
+  };
+ 
+  authService.isAuthenticated = function () {
+    return !!Session.userId;
+  };
+ 
+  authService.isAuthorized = function (authorizedRoles) {
+    if (!angular.isArray(authorizedRoles)) {
+      authorizedRoles = [authorizedRoles];
+    }
+    return (authService.isAuthenticated() &&
+      authorizedRoles.indexOf(Session.userRole) !== -1);
+  };
+ 
+  return authService;
+}).
+service('Session', function () {
+  this.create = function (sessionId, userId, userRole) {
+    this.id = sessionId;
+    this.userId = userId;
+    this.userRole = userRole;
+  };
+  this.destroy = function () {
+    this.id = null;
+    this.userId = null;
+    this.userRole = null;
+  };
+}).
+controller('LoginController', function ($scope, $rootScope, AUTH_EVENTS, AuthService) {
+  $scope.credentials = {
+    email: '',
+    password: ''
+  };
+  $scope.login = function (credentials) {
+    AuthService.login(credentials).then(function (user) {
+      $rootScope.$broadcast(AUTH_EVENTS.loginSuccess);
+      $scope.setCurrentUser(user);
+    }, function () {
+      $rootScope.$broadcast(AUTH_EVENTS.loginFailed);
+    });
+  };
+});
